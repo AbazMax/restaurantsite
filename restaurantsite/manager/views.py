3863200cde9c561033ad941bc50e37ff3c2b3588
AbazMax <maxim.abazov@gmail.com>
2022-09-22 21:31:46 +0300

    Permissions system for managers created

--- restaurantsite/manager/views.py
+++ restaurantsite/manager/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, HttpResponse, redirect
 from main.models import UserReservation, UserMessage
+from django.contrib.auth.decorators import  login_required
 
 # Create your views here.
+
+@login_required()
 def reservations_list(request):
     lst = UserReservation.objects.filter(is_processed=False)
     return render(request, 'reservations_list.html', context={
@@ -12,6 +15,7 @@ def update_reservation(request, pk):
     UserReservation.objects.filter(pk=pk).update(is_processed=True)
     return redirect('manager:reservations_list')
 
+@login_required()
 def message_list(request):
     mes_list = UserMessage.objects.filter(is_processed=False)
     return render(request, 'message_list.html', context={
